e87eabf928ca08fd6e5ec6e720e11ce2815cdff4
mitkazu <74129085+mitkazu@users.noreply.github.com>
2020-12-14 17:56:49 +0900

    Update ％TRPの計算.js

--- ％TRPの計算/％TRPの計算.js
+++ ％TRPの計算/％TRPの計算.js
@@ -5,7 +5,7 @@ document.getElementById("button").onclick = function() {
   var pp = document.getElementById("pp").value;
   var ucr = document.getElementById("ucr").value;
   var up = document.getElementById("up").value;
-  let trp= (1-ucr*up/(pp*ucr))*100
+  let trp= (1-up*pcr/(pp*ucr))*100
 
   document.getElementById("answer").innerHTML= "%TRP="+trp.toFixed(2)+"%";
 }
